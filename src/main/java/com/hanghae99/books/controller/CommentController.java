e589d857e80f33685cdefd32058ba188ac54295f
seungwook <rlatmd0829@naver.com>
2021-04-05 22:14:24 +0900

    21.04.05 22:13 Message, heartCount 구현완료

--- src/main/java/com/hanghae99/books/controller/CommentController.java
+++ src/main/java/com/hanghae99/books/controller/CommentController.java
@@ -3,14 +3,17 @@ package com.hanghae99.books.controller;
 import com.hanghae99.books.domain.Account;
 import com.hanghae99.books.domain.Book;
 import com.hanghae99.books.domain.Comment;
+import com.hanghae99.books.domain.Message;
 import com.hanghae99.books.dto.CommentRequestDto;
 import com.hanghae99.books.repository.BookRepository;
 import com.hanghae99.books.repository.CommentRepository;
 import com.hanghae99.books.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -29,7 +32,10 @@ public class CommentController {
     public ResponseEntity CreateComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long book_id, @AuthenticationPrincipal Account userDetails){
         Comment comment = commentService.CreateComment(requestDto, book_id, userDetails.getId());
         if (comment == null){
-            return ResponseEntity.badRequest().build();
+            Message message = Message.builder()
+                    .message1("댓글은 한 책에 한번만 작성가능합니다.")
+                    .build();
+            return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return ResponseEntity.ok().build();
     }
@@ -38,7 +44,10 @@ public class CommentController {
     public ResponseEntity UpdateComment(@RequestBody CommentRequestDto requestDto, @PathVariable Long book_id, @PathVariable Long comment_id, @AuthenticationPrincipal Account userDetails){
         Comment comment = commentService.UpdateComment(requestDto, comment_id, userDetails.getId());
         if (comment == null){
-            return ResponseEntity.badRequest().build();
+            Message message = Message.builder()
+                    .message1("직접 작성한 댓글만 수정할 수 있습니다.")
+                    .build();
+            return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return ResponseEntity.ok().build();
     }
@@ -47,7 +56,10 @@ public class CommentController {
     public ResponseEntity DeleteComment(@PathVariable Long book_id, @PathVariable Long comment_id, @AuthenticationPrincipal Account userDetails){
         Comment comment = commentService.DeleteComment(book_id, comment_id, userDetails.getId());
         if (comment == null){
-            return ResponseEntity.badRequest().build();
+            Message message = Message.builder()
+                    .message1("직접 작성한 댓글만 삭제할 수 있습니다.")
+                    .build();
+            return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return ResponseEntity.ok().build();
 
