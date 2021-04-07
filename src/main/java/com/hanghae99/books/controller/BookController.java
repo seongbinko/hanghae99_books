908dfefada9f7c7d7ed14a47c3a89cb055602198
seungwook <rlatmd0829@naver.com>
2021-04-07 23:58:50 +0900

    2021.04.07 23:58 리팩토링

--- src/main/java/com/hanghae99/books/controller/BookController.java
+++ src/main/java/com/hanghae99/books/controller/BookController.java
@@ -2,6 +2,7 @@ package com.hanghae99.books.controller;
 
 
 import com.hanghae99.books.domain.Book;
+import com.hanghae99.books.dto.BookRequestDto;
 import com.hanghae99.books.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,28 +22,42 @@ public class BookController {
 
 
     @GetMapping("/api/books")
-    public Page<Book> getAllBooks(
+    public Page<BookRequestDto> getAllBooks(
             @RequestParam("page") int page, // 요청페이지
             @RequestParam("size") int size, // 요청 사이즈 (게시글에 몇개씩 보여줄지)
             @RequestParam("sort") String sort // 정렬 기준 최신순, 좋아요순, 평점순
     ) {
         // Sort sortBy = Sort.by(Sort.Direction.DESC, sort);
         Pageable pageable = PageRequest.of(page-1, size);
-
+        List<BookRequestDto> bookListDto = new ArrayList<>();
         if(sort.equals("starRate")) {
             int start = (page-1) * size;
             List<Book> bookList  = bookRepository.findAllByOrderByStarRate(start, size);
-            return new PageImpl<>(bookList, pageable, bookList.size());
+
+            //return new PageImpl<>(bookList, pageable, bookList.size());
         }
 
-        if(sort.equals("heart")) {
+        else if(sort.equals("heart")) {
             int start = (page-1) * size;
             List<Book> bookList  = bookRepository.findAllByOrderByHeart(start, size);
+            for(int i=0; i<bookList.size(); i++){
+                Book book = bookList.get(i);
+                BookRequestDto requestDto = new BookRequestDto(book);
+                bookListDto.add(requestDto);
+            }
+            return new PageImpl<>(bookList, pageable, bookList.size());
+        }
+        else{
+            Page<Book> books = bookRepository.findAllByOrderByCreatedAtDesc(pageable);
+            for(int i=0; i<books.getSize(); i++){
+                Book book = books.getContent().get(i);
+                BookRequestDto requestDto = new BookRequestDto(book);
+                bookListDto.add(requestDto);
+            }
             return new PageImpl<>(bookList, pageable, bookList.size());
         }
 
-        Page<Book> books = bookRepository.findAllByOrderByCreatedAtDesc(pageable);
-        return books;
+        return null;
     }
 
     @GetMapping("/api/books/{book_id}")
--- src/main/java/com/hanghae99/books/dto/BookRequestDto.java
+++ src/main/java/com/hanghae99/books/dto/BookRequestDto.java
@@ -1,10 +1,13 @@
 package com.hanghae99.books.dto;
 
 import com.hanghae99.books.domain.Book;
+import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+@Getter
 public class BookRequestDto {
     private String imgUrl;
     private String title;
@@ -13,4 +16,5 @@ public class BookRequestDto {
         this.imgUrl = book.getImgUrl();
         this.title = book.getTitle();
     }
+
 }
